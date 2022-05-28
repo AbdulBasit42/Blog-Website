e988e7c3a01ca7d6448555f1339d0a39a8222f95
Abdul Basit <abdulbasit526492@gmail.com>
2022-05-28 13:22:37 +0530

    init

---
+++ app.js
@@ -0,0 +1,106 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const lodash = require('lodash');
+const mysql = require("mysql2");
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+var con = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE
+});
+con.connect(function (err) {
+  if (err) throw err;
+  var postsTable = "Create TABLE IF NOT EXISTS posts(title VARCHAR(110),content LONGTEXT,id int AUTO_INCREMENT,PRIMARY KEY (id))";
+  con.query(postsTable, function (err) {
+    if (err) throw err;
+  });
+});
+
+
+
+
+app.get("/", (req, res) => {
+
+  var findposts = "SELECT * FROM posts";
+  con.query(findposts, function (err, posts) {
+    res.render("home", { homeStartingContent: homeStartingContent, posts: posts });
+    // console.log(posts);
+  });
+
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+})
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+})
+app.get("/compose", (req, res) => {
+  res.render("compose");
+})
+
+
+
+app.post("/compose", (req, res) => {
+  const reqTitle = req.body.title;
+  const reqContent = req.body.post;
+  const objPost = {
+    title: reqTitle,
+    content: reqContent
+  };
+
+
+  var insertData = "INSERT INTO posts(title,content) VALUES ?";
+  var values = [
+    [reqTitle, reqContent]
+  ];
+  con.query(insertData, [values], function (err) {
+    if (err) throw err;
+  });
+
+
+  res.redirect("/");
+});
+
+app.get("/posts/:postId", (req, res) => {
+  // var lodashedUrlPostName = lodash.lowerCase(req.params.postName);
+  // console.log(req.params.postId);
+
+  var findPostId = "SELECT * FROM posts where id = ? ";
+  var valId = req.params.postId;
+
+  con.query(findPostId, [valId], function (err, post) {
+    console.log(post);
+    res.render("post", { title: post[0].title, content: post[0].content });
+  });
+
+})
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started on port 3000");
+});
